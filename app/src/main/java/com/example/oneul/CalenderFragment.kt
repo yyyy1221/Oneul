11a2937dfa02a3b40b9c54a86839f685eba38bc5
kimminju99 <mindori1019@gmail.com>
2021-05-14 05:52:21 +0900

    custom calendar v.1

--- app/src/main/java/com/example/oneul/CalenderFragment.kt
+++ app/src/main/java/com/example/oneul/CalenderFragment.kt
@@ -6,20 +6,39 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.oneul.databinding.FragmentCalenderBinding
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import com.prolificinteractive.materialcalendarview.format.DateFormatTitleFormatter
 import kotlinx.android.synthetic.main.fragment_calender.view.*
+import java.text.SimpleDateFormat
 
 class CalenderFragment: Fragment() {
     // 일정 캘린더, 일기/기분 캘린더
+
+    private lateinit var binding: FragmentCalenderBinding
+    private lateinit var CalendarView:MaterialCalendarView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_calender, container, false)
 
-        view.button_diary.setOnClickListener {
+        binding = FragmentCalenderBinding.inflate(layoutInflater)
+
+        binding.buttonDiary.setOnClickListener {
             findNavController().navigate(R.id.action_calenderFragment_to_dailyDiaryFragment)
         }
-        return view
+
+        // Custom Calendar
+        CalendarView = binding.calendarView
+
+        // 처음 오늘 날짜 표시되어있음
+        CalendarView.setSelectedDate(CalendarDay.today())
+
+
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/oneul/MainActivity.kt
+++ app/src/main/java/com/example/oneul/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.oneul
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.oneul.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonAddEventMain.setOnClickListener {
+            intent= Intent(this,AddScheduleActivity::class.java)
+            startActivity(intent)
+            // 데이터 가져와야하므로 추후에 startResultActivity로 수정해야함
+        }
     }
 }
